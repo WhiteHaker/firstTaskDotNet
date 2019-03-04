f93ad06687943fe1b013aa51031eca11e92a9d7c
Dmitriy Kohanowskij <kohanowskij@hotmail.com>
2019-03-04 20:23:18 +0300

    Initial commit

---
+++ firstTaskDotNet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TestLib;
+
+namespace firstTaskDotNet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter you name:");
+      var name = Console.ReadLine();
+
+      Console.WriteLine(Class1.GenerateOutString(name));
+      Console.ReadKey();
+    }
+  }
+}
